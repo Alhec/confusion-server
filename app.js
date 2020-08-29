45a16034fb03121237027eed8920f899cf7eea38
alhec <hector080896@gmail.com>
2020-08-29 08:51:56 -0400

    HTTPS

--- app.js
+++ app.js
@@ -32,6 +32,16 @@ connect.then((db) => {
 
 var app = express();
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -72,7 +82,6 @@ app.use('/users', usersRouter);
 // app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
