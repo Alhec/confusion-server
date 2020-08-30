d8afd392f36a0319180b0767c37c32e6155a3182
alhec <hector080896@gmail.com>
2020-08-30 10:48:04 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '233238161373832',
+        clientSecret: '6e8aaae45aa5e6c5d2583798a4792799'
+    }
 }
